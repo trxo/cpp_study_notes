f3563e1fd763d986b67fe8e252cacb92325ad008
trxo <trxo778@gmail.com>
2019-03-19 12:41:56 +0800

    edit

--- 第二章从结构到类的演变/2.9演示将美国格式的日期转换为国际格式的例子/main.cpp
+++ 第二章从结构到类的演变/2.9演示将美国格式的日期转换为国际格式的例子/main.cpp
@@ -6,12 +6,16 @@ using namespace std;
 int main()
 {
     cout << "Enter the date in American format "
-         << "(e.g, December 25,2002)";
+         << "(e.g, December 25, 2002)";
     string Date;
     getline(cin,Date,'\n');
     int i = Date.find(" ");
     string Month = Date.substr(0,i);
     int k = Date.find(",");
     string Day = Date.substr(i+1,k-i-1);
-    string Year = Date.substr(k)
+    string Year = Date.substr(k+2,4);
+    string newDate = Day + " " + Month + " " + Year;
+    cout << "Original date:" << Date << endl;
+    cout << "Converted date:" << newDate << endl;
+
 }
