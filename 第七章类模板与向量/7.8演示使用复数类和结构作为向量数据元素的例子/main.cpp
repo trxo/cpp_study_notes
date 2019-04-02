c666433eab5ad681f4a048a1c47b587e24eac935
trxo <trxo778@gmail.com>
2019-04-02 12:13:17 +0800

    edit

--- 第七章类模板与向量/7.8演示使用复数类和结构作为向量数据元素的例子/main.cpp
+++ 第七章类模板与向量/7.8演示使用复数类和结构作为向量数据元素的例子/main.cpp
@@ -4,7 +4,33 @@
 
 using namespace std;
 
+struct st
+{
+    int a,b;
+}a[] = {{2,5},{4,8}};
+
+
 int main()
 {
+    complex<float>num[] = {complex<float>(2,3),complex<float>(3.5,4.5)};
+    vector<complex<float> *>vnum(2);
+    vnum[0] = &num[0];
+    vnum[1] = &num[1];
+    for(int i = 0; i < 2; i++)
+    {
+       cout << "real is " << vnum[i]->real() << ",imag is " << vnum[i]->imag() << endl;
+    }
+    vector<st*>cp(2);
+    cp[0] = &a[0];
+    cp[1] = &a[1];
+    for(int i = 0; i < 2; i++)
+    {
+        cout << "a is " << cp[i]->a << ",b is " <<cp[i]->b << endl;
+    }
     
 }
+
+// real is 2,imag is 3
+// real is 3.5,imag is 4.5
+// a is 2,b is 5
+// a is 4,b is 8
---
+++ 第七章类模板与向量/7.9演示返回向量容量及实例的例子/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<char>a(10),b(10);
+    cout << a.empty() << "," << sizeof(b) <<  endl;
+}
